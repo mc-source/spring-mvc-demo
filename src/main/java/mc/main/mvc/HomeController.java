e013e7ce3caad23ca9a912e992de5843a66cf0ac
mc <dady120@hotmail.com>
2020-02-23 20:35:53 +0100

    initial commit

---
+++ src/main/java/mc/main/mvc/HomeController.java
@@ -0,0 +1,28 @@
+package mc.main.mvc;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("The client locale is {}.", locale);
+		
+		model.addAttribute("message", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MM yyyy HH:mm:ss",Locale.FRENCH)));
+		return "home"; 
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+
+<jsp:include page="shared/header.jsp" />
+<div role="main" class="container" style="margin-top: 100px;">
+
+	<img alt="" src='<c:url value="/static/images/spring-mvc.jpg"/>' />
+	
+</div>
+<jsp:include page="shared/footer.jsp" />
